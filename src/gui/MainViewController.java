6859d8f7cf3d73284c7dfad1990f8d1757f314f8
renan-cavalcante <renancggama@gmail.com>
2023-10-22 14:22:19 -0300

    injecao de controller no viewMain

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -3,13 +3,16 @@ package gui;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.function.Consumer;
 
 import application.Main;
+import gui.util.Alerts;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.VBox;
@@ -31,7 +34,10 @@ public class MainViewController implements Initializable {
 	
 	@FXML
 	public void onMenuClienteAction() {
-		loadView("/gui/ClienteView.fxml");
+		loadView("/gui/ClienteView.fxml", (ClienteViewController controller) -> {
+			controller.setClienteService(new Clienteservice());
+			controller.updateTableView();
+		});
 	}
 	
 	@FXML
@@ -55,7 +61,7 @@ public class MainViewController implements Initializable {
 		
 	}
 	
-	private synchronized void loadView(String absoluteName) {
+	private synchronized <T> void loadView(String absoluteName, Consumer<T> initializingAction) {
 		try {
 			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
 			VBox newVbox = loader.load();
@@ -68,12 +74,11 @@ public class MainViewController implements Initializable {
 			mainVbox.getChildren().add(mainMenu);
 			mainVbox.getChildren().addAll(newVbox.getChildren());
 			
-			ClienteViewController controller = loader.getController();
-			controller.setClienteService(new Clienteservice());
-			controller.updateTableView();
+			T controller = loader.getController();
+			initializingAction.accept(controller);
 		}
 		catch(IOException e) {
-			
+			Alerts.showAlert("IO Exception", "Erro ao carregar View", e.getMessage(), AlertType.ERROR);
 		}
 	}
 
